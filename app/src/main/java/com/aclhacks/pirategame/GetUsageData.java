28eedddcc7ea35f7b5cc056864a3f2a5374e94ff
Avni <2025agarg@tjhsst.edu>
2023-05-20 14:18:27 -0400

    user permission

--- app/src/main/java/com/aclhacks/pirategame/GetUsageData.java
+++ app/src/main/java/com/aclhacks/pirategame/GetUsageData.java
@@ -1,25 +1,21 @@
 package com.aclhacks.pirategame;
 
-import androidx.appcompat.app.AppCompatActivity;
+import static androidx.core.content.ContextCompat.getSystemService;
+import static androidx.core.content.ContextCompat.startActivity;
 
 import android.app.AppOpsManager;
+import android.app.usage.*;
 import android.content.Context;
 import android.content.Intent;
-import android.os.Bundle;
 import android.provider.Settings;
-import android.view.View;
-import android.widget.Button;
-import androidx.appcompat.app.AppCompatActivity;
 
-public class MainActivity extends AppCompatActivity {
-    // Declare your variables and views here
+import java.util.List;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.button_layout);
+public class GetUsageData {
 
-    }
+//    public float total_time(long start, long end) {
+//        final List<UsageStats> queryUsageStats = UsageStatsManager.queryUsageStats(start, end);
+//    }
 
 //    private void requestUsageStatsPermission() {
 //        if (!hasUsageStatsPermission()) {
@@ -34,5 +30,8 @@ public class MainActivity extends AppCompatActivity {
 //        return mode == AppOpsManager.MODE_ALLOWED;
 //    }
 
-    // Create additional methods for various app functionalities
+//    AppOpsManager.OPSTR_GET_USAGE_STATS = "android:get_usage_stats";
+
+
+
 }
