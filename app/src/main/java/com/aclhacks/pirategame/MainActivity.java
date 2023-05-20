7614592b6f3abf484a9b56b6b14acd9a498526b8
rishabh <rishabhgoel0213@gmail.com>
2023-05-20 16:57:19 -0400

    open street map

--- app/src/main/java/com/aclhacks/pirategame/MainActivity.java
+++ app/src/main/java/com/aclhacks/pirategame/MainActivity.java
@@ -2,15 +2,30 @@ package com.aclhacks.pirategame;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.app.AppOpsManager;
 import android.content.*;
+import android.content.pm.PackageManager;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.provider.Settings;
+import android.support.v13.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.util.Patterns;
+import android.view.View;
 import android.widget.*;
 import androidx.annotation.Nullable;
 
+
+import android.Manifest;
+import android.content.Context;
+
+import org.osmdroid.config.Configuration;
+import org.osmdroid.tileprovider.tilesource.TileSourceFactory;
+import org.osmdroid.views.MapView;
+
+import java.util.ArrayList;
 
 
 public class MainActivity extends AppCompatActivity
@@ -24,7 +39,10 @@ public class MainActivity extends AppCompatActivity
     private EditText username;
 
     private static final int REQUEST_USAGE_STATS = 1;
+    private final int REQUEST_MAPS = 1;
+    private MapView map = null;
 
+    @SuppressLint({"ResourceType", "MissingInflatedId"})
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
@@ -36,21 +54,46 @@ public class MainActivity extends AppCompatActivity
         passwordConfirm = findViewById(R.id.passwordConfirmation);
         fullName = findViewById(R.id.fullname);
         username = findViewById(R.id.username);
-        signupButton.setOnClickListener(v -> {
-            if (password.getText().toString().equals(passwordConfirm.getText().toString()))
+        signupButton.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view)
             {
-                createAccount(username.getText().toString(), password.getText().toString(), email.getText().toString(), fullName.getText().toString());
+                //set content view to map
+                if (password.getText().toString().equals(passwordConfirm.getText().toString()))
+                {
+                    createAccount(username.getText().toString(), password.getText().toString(), email.getText().toString(), fullName.getText().toString());
+                }
             }
         });
 
-        if (!hasUsageStatsPermission()) {
-            requestUsageStatsPermission();
-        } else {
-            // Permission already granted, proceed with retrieving usage stats
-            retrieveUsageStats();
-        }
     }
 
+    @Override
+    public void onResume() {
+        super.onResume();
+        //this will refresh the osmdroid configuration on resuming.
+        //if you make changes to the configuration, use
+        //SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
+        //Configuration.getInstance().load(this, PreferenceManager.getDefaultSharedPreferences(this));
+        map.onResume(); //needed for compass, my location overlays, v6.0.0 and up
+    }
+
+    @SuppressLint({"ResourceType", "MissingInflatedId"})
+    private void showMap()
+    {
+        Context ctx = getApplicationContext();
+        Configuration.getInstance().load(ctx, PreferenceManager.getDefaultSharedPreferences(ctx));
+        setContentView(R.layout.map_view);
+
+
+        map = (MapView) findViewById(R.layout.map_view);
+        map.setTileSource(TileSourceFactory.MAPNIK);
+
+        requestPermissionsIfNecessary(new String[]{
+                Manifest.permission.WRITE_EXTERNAL_STORAGE
+        });
+    }
     // request permission to collect screen time stats
     private boolean hasUsageStatsPermission() {
         AppOpsManager appOps = (AppOpsManager) getSystemService(Context.APP_OPS_SERVICE);
@@ -113,7 +156,24 @@ public class MainActivity extends AppCompatActivity
         long newRowId2 = db2.insert(LoginInfo.UserEntry.TABLE_NAME, null, values2);
     }
 
-    private boolean validateEmail(String email)
+    private void requestPermissionsIfNecessary(String[] permissions) {
+        ArrayList<String> permissionsToRequest = new ArrayList<>();
+        for (String permission : permissions) {
+            if (ContextCompat.checkSelfPermission(this, permission)
+                    != PackageManager.PERMISSION_GRANTED) {
+                // Permission is not granted
+                permissionsToRequest.add(permission);
+            }
+        }
+        if (permissionsToRequest.size() > 0) {
+            ActivityCompat.requestPermissions(
+                    this,
+                    permissionsToRequest.toArray(new String[0]),
+                    REQUEST_MAPS);
+        }
+    }
+
+    private static boolean validateEmail(String email)
     {
         return !email.isEmpty() && Patterns.EMAIL_ADDRESS.matcher(email).matches();
     }
