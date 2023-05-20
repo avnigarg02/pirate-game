34278943299d9082af7e6e9cb2e823f6ffdd40eb
rishabh <rishabhgoel0213@gmail.com>
2023-05-20 11:57:31 -0400

    test ui

--- app/src/main/java/com/aclhacks/pirategame/MainActivity.java
+++ app/src/main/java/com/aclhacks/pirategame/MainActivity.java
@@ -3,12 +3,66 @@ package com.aclhacks.pirategame;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
+    // Declare your variables and views here
+    private Button startButton;
+    private Button defendButton;
+    private Button upgradeButton;
+    private Button collectButton;
+    private Button challengeButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.button_layout);
+
+        // Initialize your views
+        startButton = findViewById(R.id.myButton);
+//        defendButton = findViewById(R.id.myButton);
+//        upgradeButton = findViewById(R.id.myButton);
+//        collectButton = findViewById(R.id.myButton);
+//        challengeButton = findViewById(R.id.myButton);
+
+        // Set up click listeners for the buttons
+        startButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Implement logic for starting a productivity session
+            }
+        });
+
+//        defendButton.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                // Implement logic for defending against distractions
+//            }
+//        });
+//
+//        upgradeButton.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                // Implement logic for unlocking pirate upgrades
+//            }
+//        });
+//
+//        collectButton.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                // Implement logic for collecting booty
+//            }
+//        });
+//
+//        challengeButton.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                // Implement logic for pirate challenges
+//            }
+//        });
     }
+
+    // Create additional methods for various app functionalities
 }
