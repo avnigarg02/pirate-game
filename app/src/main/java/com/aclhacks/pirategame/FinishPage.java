493e61384eab672f1876e7c497305ac346c8d5c9
rishabh <rishabhgoel0213@gmail.com>
2023-05-21 10:11:19 -0400

    add the coins

--- app/src/main/java/com/aclhacks/pirategame/FinishPage.java
+++ app/src/main/java/com/aclhacks/pirategame/FinishPage.java
@@ -3,6 +3,7 @@ package com.aclhacks.pirategame;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.*;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.provider.Settings;
@@ -30,11 +31,45 @@ public class FinishPage extends AppCompatActivity {
         coinsText.setText(String.format("+%d coins", coins));
 
         long userId = getIntent().getLongExtra("userId", 0);
+
+        //read current amount of coins
+        UserDataDbHelper mDbHelper2 = new UserDataDbHelper(this);
+        SQLiteDatabase db2 = mDbHelper2.getReadableDatabase();
+
+        String[] projection2 = {
+                UserInfo.UserEntry.FULL_NAME,
+                UserInfo.UserEntry.EMAIL,
+                UserInfo.UserEntry.ALLOWED_APPS,
+                UserInfo.UserEntry.BLOCKED_APPS,
+                UserInfo.UserEntry.USAGE_HISTORY,
+                UserInfo.UserEntry.COINS
+        };
+
+        String selection2 = UserInfo.UserEntry._ID + " = ?";
+        String[] selectionArgs2 = { String.valueOf(userId) };
+
+
+        Cursor cursor2 = db2.query(
+                UserInfo.UserEntry.TABLE_NAME,
+                projection2,
+                selection2,
+                selectionArgs2,
+                null,
+                null,
+                null
+        );
+
+        cursor2.moveToFirst();
+        int currCoins = cursor2.getInt(cursor2.getColumnIndexOrThrow(UserInfo.UserEntry.COINS));
+        cursor2.close();
+
+        //add coins to current amount of coins
         UserDataDbHelper mDbHelper = new UserDataDbHelper(this);
         SQLiteDatabase db = mDbHelper.getWritableDatabase();
         ContentValues values = new ContentValues();
-        values.put(UserInfo.UserEntry.COINS, coins);
+        values.put(UserInfo.UserEntry.COINS, coins + currCoins);
         db.update(UserInfo.UserEntry.TABLE_NAME, values, "_id = " + userId, null);
+
         home.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
