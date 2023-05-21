5132b9089e8ed1cf8fbcbbd990724812fa6c3e40
rishabh <rishabhgoel0213@gmail.com>
2023-05-20 21:57:13 -0400

    changes

--- app/src/main/java/com/aclhacks/pirategame/Path.java
+++ app/src/main/java/com/aclhacks/pirategame/Path.java
@@ -3,21 +3,32 @@ package com.aclhacks.pirategame;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.pm.PackageManager;
+import android.content.res.AssetManager;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+
+import androidx.core.content.res.ResourcesCompat;
 import androidx.preference.PreferenceManager;
 
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 
+import org.osmdroid.api.IMapController;
 import org.osmdroid.config.Configuration;
 import org.osmdroid.tileprovider.tilesource.TileSourceFactory;
-import org.osmdroid.views.MapView;
 import org.osmdroid.util.GeoPoint;
-import org.osmdroid.views.overlay.*;
-//import org.osmdroid.views.overlay.kml.*;
+import org.osmdroid.views.MapView;
+import org.osmdroid.views.overlay.Marker;
 import android.Manifest;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 import android.content.Context;
 
 
@@ -27,8 +38,7 @@ public class Path extends AppCompatActivity {
     private MapView map = null;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState)
-    {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         Context ctx = getApplicationContext();
         Configuration.getInstance().load(ctx, PreferenceManager.getDefaultSharedPreferences(ctx));
@@ -42,11 +52,31 @@ public class Path extends AppCompatActivity {
                 Manifest.permission.WRITE_EXTERNAL_STORAGE
         });
 
-//        IMapController mapController = map.getController();
-//        mapController.setZoom(9.5);
-//        GeoPoint startPoint = new GeoPoint(38.8977, -77.0365);
-//        mapController.setCenter(startPoint);
+        AssetManager assetManager = getAssets();
+        InputStream inputStream;
+        try {
+            inputStream = assetManager.open("Coords.csv");
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
 
+        Scanner coords = new Scanner(inputStream, "UTF-8");
+        List<List<Double>> points = new ArrayList<>();
+        while (coords.hasNextLine())
+        {
+            String line = coords.nextLine();
+            String[] lineArr = line.split(",");
+            List<Double> point = new ArrayList<>();
+            point.add(Double.parseDouble(lineArr[0]));
+            point.add(Double.parseDouble(lineArr[1]));
+            points.add(point);
+        }
+        List<Double> randPoint = points.get((int) (Math.random() * points.size()));
+
+        IMapController mapController = map.getController();
+        mapController.setZoom(9.5);
+        GeoPoint startPoint = new GeoPoint(randPoint.get(0), randPoint.get(1));
+        mapController.setCenter(startPoint);
     }
 
     @Override
