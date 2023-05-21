744e2019ad25700647d07d07695856434b0aec04
Avni <2025agarg@tjhsst.edu>
2023-05-21 10:06:07 -0400

    logout

--- app/src/main/java/com/aclhacks/pirategame/SettingsPage.java
+++ app/src/main/java/com/aclhacks/pirategame/SettingsPage.java
@@ -12,11 +12,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class SettingsPage extends AppCompatActivity {
 
-    private Button changePassword;
-    private Button changeUsername;
-    private Button changeEmail;
     private Button statistics;
     private Button friends;
+    private Button changePassword;
+    private Button changeEmail;
+    private Button logout;
     private Button back;
     private TextView coinsText;
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,7 +25,7 @@ public class SettingsPage extends AppCompatActivity {
 
         back = findViewById(R.id.back);
         changePassword = findViewById(R.id.changePassword);
-        changeUsername = findViewById(R.id.changeUsername);
+        logout = findViewById(R.id.logout);
         changeEmail = findViewById(R.id.changeEmail);
         statistics = findViewById(R.id.statistics);
         friends = findViewById(R.id.friends);
@@ -71,8 +71,15 @@ public class SettingsPage extends AppCompatActivity {
         back.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-//                Intent intent = new Intent(SettingsPage.this, ChoicePage.class);
-//                startActivity(intent);
+                finish();
+            }
+        });
+
+        logout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(SettingsPage.this, LoginPage.class);
+                startActivity(intent);
                 finish();
             }
         });
