0a3ec662fef93682ce465f89964825fc59b020d3
Avni <2025agarg@tjhsst.edu>
2023-05-21 09:41:50 -0400

    bootup then go to login

--- app/src/main/java/com/aclhacks/pirategame/BootupPage.java
+++ app/src/main/java/com/aclhacks/pirategame/BootupPage.java
@@ -1,5 +1,6 @@
 package com.aclhacks.pirategame;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -16,6 +17,8 @@ public class BootupPage extends AppCompatActivity {
             public void run() {
                 try {
                     sleep(1000);
+                    Intent intent = new Intent(BootupPage.this, LoginPage.class);
+                    startActivity(intent);
                     finish();
                 } catch (InterruptedException e) {
                     e.printStackTrace();
