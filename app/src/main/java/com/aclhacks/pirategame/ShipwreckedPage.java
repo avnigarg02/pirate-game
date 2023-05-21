08677a9522749f9218c297baca3a167fd7cc2558
Avni <2025agarg@tjhsst.edu>
2023-05-21 09:21:39 -0400

    shipwreckedpage linked

--- app/src/main/java/com/aclhacks/pirategame/ShipwreckedPage.java
+++ app/src/main/java/com/aclhacks/pirategame/ShipwreckedPage.java
@@ -1,15 +1,43 @@
 package com.aclhacks.pirategame;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.*;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class ShipwreckedPage extends AppCompatActivity
 {
+
+    private ImageButton settings;
+    private Button home;
+
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.fail);
+
+        settings = findViewById(R.id.settings);
+        home = findViewById(R.id.home);
+
+        settings.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(ShipwreckedPage.this, SettingsPage.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
+        home.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(ShipwreckedPage.this, ChoicePage.class);
+                startActivity(intent);
+                finish();
+            }
+        });
     }
 }
